7479691a786132e9ec0142bec1db7fef90141506
Alex Vorsa <aleksandr.vorsa@gmail.com>
2022-08-22 21:22:41 +0300

    update url

--- tests/helper/base_app.py
+++ tests/helper/base_app.py
@@ -14,7 +14,7 @@ class BasePage:
 
     def __init__(self, driver):
         self.driver = driver
-        self.base_url = "http://localhost"
+        self.base_url = "https://alexvorsa.github.io"
 
     def find_element(self, locator, time=10):
         """
